7fc60c613a1edf34147c933f191c5980a695782a
reksa <reksa.ariansyah@gmail.com>
2026-03-01 13:56:13 +0700

    pertemuan 1

---
+++ Pertemuan 1/Project QR/index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL?",
+      name: "url",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.url;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+    console.log("QR code has been generated!");
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
